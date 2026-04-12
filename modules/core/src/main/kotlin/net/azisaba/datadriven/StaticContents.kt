576a5a17a25210c30e3ae2caf28d0ff27e95a6c7
tksimeji <tksimeji@outlook.com>
2026-04-12 20:11:46 +0900

    feat: add StaticContents<T>

---
+++ modules/core/src/main/kotlin/net/azisaba/datadriven/StaticContents.kt
@@ -0,0 +1,37 @@
+package net.azisaba.datadriven
+
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
+
+abstract class StaticContents<T : Any> : Contents<T> {
+    private val byKey: ConcurrentMap<ContentKey<T>, T> = ConcurrentHashMap()
+    private val byValue: ConcurrentMap<T, ContentKey<T>> = ConcurrentHashMap()
+
+    init {
+        val builder = BindingBuilder(byKey, byValue)
+        builder.bootstrap()
+    }
+
+    override fun byKey(key: ContentKey<T>): T? = byKey[key]
+
+    override fun keyOf(value: T): ContentKey<T>? = byValue[value]
+
+    override fun all(): Collection<T> = byKey.values
+
+    protected abstract fun BindingBuilder<T>.bootstrap()
+
+    protected class BindingBuilder<T : Any>(
+        private val byKey: MutableMap<ContentKey<T>, T>,
+        private val byValue: MutableMap<T, ContentKey<T>>,
+    ) {
+        fun <V : T> bind(key: ContentKey<T>, value: V): V {
+            require(byKey.putIfAbsent(key, value) == null) {
+                "Duplicate content key detected: $key"
+            }
+            require(byValue.putIfAbsent(value, key) == null) {
+                "Duplicate value detected: $value"
+            }
+            return value
+        }
+    }
+}
