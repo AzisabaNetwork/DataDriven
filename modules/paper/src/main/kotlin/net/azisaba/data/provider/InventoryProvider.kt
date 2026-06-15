687eba7b03cedbd2bf9f10c821a56767e7afc97f
tksimeji <tksimeji@outlook.com>
2026-06-15 12:51:18 +0900

    fix: implement Examinable in InventoryProvider to support property inspection

--- modules/paper/src/main/kotlin/net/azisaba/data/provider/InventoryProvider.kt
+++ modules/paper/src/main/kotlin/net/azisaba/data/provider/InventoryProvider.kt
@@ -1,11 +1,21 @@
 package net.azisaba.data.provider
 
 import kotlinx.serialization.Serializable
+import net.kyori.examination.Examinable
+import net.kyori.examination.ExaminableProperty
 import org.bukkit.inventory.Inventory
+import java.util.stream.Stream
 import kotlin.random.Random
 
 @Serializable
-data class InventoryProvider(val rolls: IntProvider, val items: ChoiceProvider<ItemStackProvider>) {
+data class InventoryProvider(val rolls: IntProvider, val items: ChoiceProvider<ItemStackProvider>) : Examinable {
+    override fun examinableProperties(): Stream<out ExaminableProperty?> {
+        return Stream.of(
+            ExaminableProperty.of("rolls", rolls),
+            ExaminableProperty.of("items", items),
+        )
+    }
+
     fun fillInventory(inventory: Inventory, random: Random = Random.Default) {
         val rollCount = rolls.provide(random)
         val availableSlots = (0 until inventory.size).toMutableList()
